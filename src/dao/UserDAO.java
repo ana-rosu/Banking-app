ae583c8cee16220805f91f3f1de010fd85169ad5
anarosu <anarosu766@gmail.com>
2024-05-22 20:11:49 +0300

    fix: updated UserDAO to insert/update/delete its associated entities too

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -6,17 +6,22 @@ import model.user.Address;
 import model.user.User;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements GenericDAO<User> {
     private final Connection connection;
-    public UserDAO(Connection connection){
+    private AddressDAO addressDAO;
+    private AccountDAO accountDAO;
+    public UserDAO(Connection connection, AddressDAO addressDAO, AccountDAO accountDAO){
         this.connection = connection;
+        this.addressDAO = addressDAO;
+        this.accountDAO = accountDAO;
     }
     @Override
     public void create(User user) {
         try {
-            PreparedStatement stmt = connection.prepareStatement("INSERT INTO usr (firstName, lastName, email, passwrd, phoneNumber, dateOfBirth, addressId) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO Usr (firstName, lastName, email, passwrd, phoneNumber, dateOfBirth, addressId) VALUES (?, ?, ?, ?, ?, ?, ?)");
             this.setParameters(stmt,  user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword(), user.getPhoneNumber(), new java.sql.Date(user.getDateOfBirth().getTime()), user.getAddress().getId());
             stmt.executeUpdate();
 
@@ -30,6 +35,14 @@ public class UserDAO implements GenericDAO<User> {
                     }
                 }
             }
+
+            // insert address
+            addressDAO.create(user.getAddress());
+            // insert accounts
+            for (Account account : user.getAccountList()) {
+                account.setUserId(user.getId());
+                accountDAO.create(account);
+            }
         } catch (SQLException e) {
             System.err.println("Error creating user: " + e.getMessage());
         }
@@ -37,44 +50,54 @@ public class UserDAO implements GenericDAO<User> {
 
     @Override
     public User read(int id) {
-        String sql = "SELECT * FROM usr WHERE id = ?";
+        String sql = "SELECT * FROM Usr WHERE id = ?";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setInt(1, id);
             try (ResultSet resultSet = stmt.executeQuery()) {
                 if (resultSet.next()) {
-                    Address addr = getAddress(resultSet.getInt("addressId"));
-                    List<Account> accList = getAccountsForUser(id);
-                    return new User(resultSet.getInt("id"), resultSet.getString("firstName"), resultSet.getString("lastName"), resultSet.getString("email"), resultSet.getString("phoneNumber"), resultSet.getDate("dateOfBirth"), addr, accList); // create a User object from the retrieved data
+                    Address addr = addressDAO.read(resultSet.getInt("addressId"));
+                    List<Account> accList = accountDAO.selectAllWhereUserId(id);
+                    return new User(
+                            resultSet.getInt("id"),
+                            resultSet.getString("firstName"),
+                            resultSet.getString("lastName"),
+                            resultSet.getString("email"),
+                            resultSet.getString("phoneNumber"),
+                            resultSet.getDate("dateOfBirth"),
+                            addr,
+                            accList); // create a User object from the retrieved data
                 } else {
-                    return null;  // card with the specified id was not found
+                    return null;
                 }
             }
         } catch (SQLException e) {
-            System.err.println("Error reading card: " + e.getMessage());
+            System.err.println("Error reading user: " + e.getMessage());
             return null;
         }
     }
-    private Address getAddress(int addressId) throws SQLException {
-        AddressDAO addressDAO = new AddressDAO(connection);
-        return addressDAO.read(addressId);
-    }
-
-    private List<Account> getAccountsForUser(int userId) throws SQLException {
-        AccountDAO accountDAO = new AccountDAO(connection);
-        return accountDAO.selectAllWhereUserId(userId);
-    }
     public void update(User user) {
-        String sql = "UPDATE usr SET firstName = ?, lastName = ?, email = ?, passwrd = ?, phoneNumber = ?, dateOfBirth = ?, addressId = ? WHERE id = ?";
+        String sql = "UPDATE Usr SET firstName = ?, lastName = ?, email = ?, passwrd = ?, phoneNumber = ?, dateOfBirth = ?, addressId = ? WHERE id = ?";
         try(PreparedStatement stmt = connection.prepareStatement(sql)){
             this.setParameters(stmt, user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword(), user.getPhoneNumber(), new java.sql.Date(user.getDateOfBirth().getTime()), user.getAddress().getId());
             stmt.executeUpdate();
+            // update address
+            addressDAO.update(user.getAddress());
+            // update accounts
+            for (Account account : user.getAccountList()) {
+                accountDAO.update(account);
+            }
         }catch(SQLException e){
             System.err.println("Error updating user: " + e.getMessage());
         }
     }
 
     public void delete(int id) {
-        String sql = "DELETE FROM usr WHERE id = ?";
+        List<Account> accounts = accountDAO.selectAllWhereUserId(id);
+        // delete associated accounts
+        for (Account account : accounts) {
+            accountDAO.delete(account);
+        }
+        String sql = "DELETE FROM Usr WHERE id = ?";
         try(PreparedStatement stmt = connection.prepareStatement(sql)){
             stmt.setInt(1, id);
             stmt.executeUpdate();
@@ -82,6 +105,22 @@ public class UserDAO implements GenericDAO<User> {
         catch(SQLException e){
             System.err.println("Error deleting user: " + e.getMessage());
         }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT id FROM Usr";
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+             while (rs.next()){
+                 int id = rs.getInt(1);
+                 users.add(this.read(id));
+             }
+        }
+        catch(SQLException e){
+            System.err.println("Error fetching users: " + e.getMessage());
+        }
+        return users;
     }
 
 }
